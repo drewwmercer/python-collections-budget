117809a1bb074052fea6380668f64936371f234a
Drew Mercer <japanexpress@gmail.com>
2023-01-11 13:29:51 -0500

    All 12 tests in module2 passing

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,3 +1,4 @@
+from cmath import exp
 from . import Expense
 
 class BudgetList:
@@ -21,3 +22,15 @@ class BudgetList:
     
 def main():
     myBudgetList = BudgetList(1200)
+    
+    expenses = Expense.Expenses()
+    
+    expenses.read_expenses('data/spending_data.csv')
+    
+    for expense in expenses.list:
+        myBudgetList.append(expense.amount)
+        
+    print('The count of all expenses: ' + str(len(myBudgetList)))
+    
+if __name__ == '__main__':
+    main()
